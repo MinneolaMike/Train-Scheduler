1ebb95ac79d9e6b3f6d681456f7357a6d88faa5f
chefgonzo <chefgonzo@gmail.com>
2018-07-31 19:35:29 -0400

    Html page set-up with all links -- Firebase initiated on main.js

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAWCjx49FBw4obWw9wJPNPZiGXo-3dS7ls",
+    authDomain: "train-scheduler-bfead.firebaseapp.com",
+    databaseURL: "https://train-scheduler-bfead.firebaseio.com",
+    projectId: "train-scheduler-bfead",
+    storageBucket: "train-scheduler-bfead.appspot.com",
+    messagingSenderId: "7919516478"
+  };
+  firebase.initializeApp(config);
+
+  
